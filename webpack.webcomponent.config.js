820c2dc3ed0cb88c7600f79ee33c5f64b440f5c0
Lester Hsu <lesterhsu0205@hotmail.com>
2025-06-19 02:20:51 +0800

    更新 package.json 中的腳本以使用 pnpm，調整 webpack 輸出路徑至 public/js，並修改測試頁面以引入新的 Web Component URL。此外，刪除不再需要的 HTML 檔案和 SVG 檔案。

--- webpack.webcomponent.config.js
+++ webpack.webcomponent.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production', // 'production', 'development', 'none'
   entry: './src/webComponent.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/js'),
     filename: 'elf-webcomponent.js',
     library: 'ELFWebComponent',
     libraryTarget: 'umd',
