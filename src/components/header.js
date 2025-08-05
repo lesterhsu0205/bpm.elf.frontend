c08bc4ea3817d2df50cb7dd0bba46a8fce9f7390
Lester Hsu <lesterhsu0205@hotmail.com>
2025-08-05 13:56:24 +0800

    更新 .gitignore 以排除 chrome_extension/elf-chrome-component.js，刪除不再使用的 favicon.ico，新增 magician-hat.png，並在 header.js 中更新導航欄以顯示新圖示，調整 layout.js 中的圖示連結，最後修改 globals.css 中的邊距設置。

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,8 @@ const Header = () => (
     className="header-custom"
   >
     <Container fluid className="p-0">
-      <Navbar.Brand as={Link} href="/">
+      <Navbar.Brand as={Link} href="/" style={{display: 'flex', alignItems: 'center'}}>
+        <img src="/bpm-elf/magician-hat512.png" alt="BPM ELF Logo" style={{height: '32px', marginRight: '8px'}} />
         BPM ELF
       </Navbar.Brand>
       {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
